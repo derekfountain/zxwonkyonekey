fe171c5dbc79f5ea886a49e3ed0fce0148ff1d87
Derek Fountain <derek@scratters.com>
2018-06-29 11:52:04 +0100

    add a licence

--- src/runner_sprite.asm
+++ src/runner_sprite.asm
@@ -1,3 +1,11 @@
+;; This file was originally sourced from the Z88DK project and
+;; hence is licenced under the Clarified Artistic License.
+;;
+;; It was originally written by the same author as ZX Wonky One Key
+;; and the original copyright therefore remains.
+;;
+;; (C) Copyright Derek Fountain 2018
+	
 SECTION rodata_user
 
 	defb @00000000
