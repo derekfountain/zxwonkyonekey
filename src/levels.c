79e6642cb81f2aa97db9d4974a6c53a742b47b7d
Derek Fountain <derek@scratters.com>
2018-08-06 14:05:52 +0100

    first stab at UDGs in the level

--- src/levels.c
+++ src/levels.c
@@ -22,15 +22,24 @@
 
 #include "levels.h"
 
+extern uint8_t grass1[];
+#define GRASS ('\x80')
+
+extern uint8_t jumper[];
+#define JUMPER ('\x81')
+
+extern uint8_t finish[];
+#define FINISH ('\x82')
+
 LEVEL_DATA level_data[] = {
 
   { level0,
     START_POINT(0,135),
     LEVEL_ATT_DATA("Background", INK_BLACK|PAPER_WHITE,
                    "Solid",      INK_GREEN|PAPER_WHITE,
-                   "Jumper",     INK_RED|PAPER_YELLOW,
+                   "Jumper",     INK_RED|PAPER_GREEN,
                    "Killer",     INK_BLUE|PAPER_WHITE,
-                   "Finish",     INK_YELLOW|PAPER_WHITE) },
+                   "Finish",     INK_YELLOW|PAPER_BLUE) },
 };
 
 LEVEL_DATA* get_level_data( uint8_t level )
@@ -50,97 +59,101 @@ void level0(void)
    * I currently have no idea how I'm going to do this.
    */
 
+  sp1_TileEntry(128, grass1);
+  sp1_TileEntry(129, jumper);
+  sp1_TileEntry(130, finish);
+
   /* Bottom row */
-  sp1_PrintAt(22,0,solid_att,'X');
-  sp1_PrintAt(22,31,solid_att,'X');
+  sp1_PrintAt(22,0,solid_att,GRASS);
+  sp1_PrintAt(22,31,solid_att,GRASS);
   for(i=0;i<32;i++)
-    sp1_PrintAt(23,i,solid_att,'X');
+    sp1_PrintAt(23,i,solid_att,GRASS);
 
 
   /* Central area, 1 jump */
-  sp1_PrintAt(23,17,jumper_att,'Y');
+  sp1_PrintAt(23,17,jumper_att,JUMPER);
 
   /* Step 1 */
   for(i=9;i<14;i++)
-    sp1_PrintAt(21,i,solid_att,'X');
-  sp1_PrintAt(21,8,jumper_att,'Y');
+    sp1_PrintAt(21,i,solid_att,GRASS);
+  sp1_PrintAt(21,8,jumper_att,JUMPER);
 
   /* Hop */
-  sp1_PrintAt(19,4,solid_att,'X');
-  sp1_PrintAt(19,5,solid_att,'X');
-  sp1_PrintAt(19,6,jumper_att,'Y');
+  sp1_PrintAt(19,4,solid_att,GRASS);
+  sp1_PrintAt(19,5,solid_att,GRASS);
+  sp1_PrintAt(19,6,jumper_att,JUMPER);
 
   /* Step 2 */  
   for(i=10;i<15;i++)
-    sp1_PrintAt(18,i,solid_att,'X');
-  sp1_PrintAt(18,15,jumper_att,'Y');
+    sp1_PrintAt(18,i,solid_att,GRASS);
+  sp1_PrintAt(18,15,jumper_att,JUMPER);
 
   /* Single jump */
-  sp1_PrintAt(16,19,solid_att,'X');
-  sp1_PrintAt(16,20,jumper_att,'Y');
+  sp1_PrintAt(16,19,solid_att,GRASS);
+  sp1_PrintAt(16,20,jumper_att,JUMPER);
 
   /* Step 3 */
   for(i=23;i<26;i++)
-    sp1_PrintAt(14,i,solid_att,'X');
-  sp1_PrintAt(14,22,jumper_att,'Y');
+    sp1_PrintAt(14,i,solid_att,GRASS);
+  sp1_PrintAt(14,22,jumper_att,JUMPER);
 
 
   /* Rest 1 */
   for(i=10;i<20;i++)
-    sp1_PrintAt(13,i,solid_att,'X');
-  sp1_PrintAt(12,10,solid_att,'X');
-  sp1_PrintAt(12,19,solid_att,'X');
-  sp1_PrintAt(13,11,jumper_att,'Y');
+    sp1_PrintAt(13,i,solid_att,GRASS);
+  sp1_PrintAt(12,10,solid_att,GRASS);
+  sp1_PrintAt(12,19,solid_att,GRASS);
+  sp1_PrintAt(13,11,jumper_att,JUMPER);
 
 
   /* Step 4 and 5 */
   for(i=5;i<8;i++)
-    sp1_PrintAt(11,i,solid_att,'X');
-  sp1_PrintAt(11,8,jumper_att,'Y');
-  sp1_PrintAt(11,9,solid_att,'X');
+    sp1_PrintAt(11,i,solid_att,GRASS);
+  sp1_PrintAt(11,8,jumper_att,JUMPER);
+  sp1_PrintAt(11,9,solid_att,GRASS);
 
   for(i=4;i<6;i++)
-    sp1_PrintAt(9,i,solid_att,'X');
-  sp1_PrintAt(9,6,jumper_att,'Y');
+    sp1_PrintAt(9,i,solid_att,GRASS);
+  sp1_PrintAt(9,6,jumper_att,JUMPER);
 
 
   /* Step 6 (long with gap) */
   for(i=10;i<24;i++)
-    sp1_PrintAt(7,i,solid_att,'X');
-  sp1_PrintAt(7,21,background_att,'X');
+    sp1_PrintAt(7,i,solid_att,GRASS);
+  sp1_PrintAt(7,21,background_att,' ');
 
 
   /* Step 7 */
   for(i=21;i<23;i++)
-    sp1_PrintAt(10,i,solid_att,'X');
-  sp1_PrintAt(10,23,jumper_att,'Y');
+    sp1_PrintAt(10,i,solid_att,GRASS);
+  sp1_PrintAt(10,23,jumper_att,JUMPER);
 
   /* Step 8 */
   for(i=26;i<27;i++)
-    sp1_PrintAt(8,i,solid_att,'X');
-  sp1_PrintAt(8,27,jumper_att,'Y');
+    sp1_PrintAt(8,i,solid_att,GRASS);
+  sp1_PrintAt(8,27,jumper_att,JUMPER);
 
   /* Side wall */
-  sp1_PrintAt(3,31,solid_att,'X');
-  sp1_PrintAt(4,31,solid_att,'X');
-  sp1_PrintAt(5,31,solid_att,'X');
-  sp1_PrintAt(6,31,solid_att,'X');
-  sp1_PrintAt(7,31,solid_att,'X');
-  sp1_PrintAt(7,30,jumper_att,'Y');
-  sp1_PrintAt(7,29,solid_att,'X');
-  sp1_PrintAt(7,28,solid_att,'X');
+  sp1_PrintAt(3,31,solid_att,GRASS);
+  sp1_PrintAt(4,31,solid_att,GRASS);
+  sp1_PrintAt(5,31,solid_att,GRASS);
+  sp1_PrintAt(6,31,solid_att,GRASS);
+  sp1_PrintAt(7,31,solid_att,GRASS);
+  sp1_PrintAt(7,30,jumper_att,JUMPER);
+  sp1_PrintAt(7,29,solid_att,GRASS);
+  sp1_PrintAt(7,28,solid_att,GRASS);
 
   /* Step 8 */
-  sp1_PrintAt(5,26,jumper_att,'Y');
+  sp1_PrintAt(5,26,jumper_att,JUMPER);
   for(i=3;i<25;i++)
-    sp1_PrintAt(4,i,solid_att,'X');
-  sp1_PrintAt(3,5,solid_att,'X');
-  sp1_PrintAt(4,9,background_att,'X');
-  sp1_PrintAt(4,10,jumper_att,'Y');
+    sp1_PrintAt(4,i,solid_att,GRASS);
+  sp1_PrintAt(3,5,solid_att,GRASS);
+  sp1_PrintAt(4,9,background_att,' ');
+  sp1_PrintAt(4,10,jumper_att,JUMPER);
 
 
   /* Finish */
   for(i=0;i<3;i++)
-    sp1_PrintAt(16,i,solid_att,'X');
-  sp1_PrintAt(15,0,finish_att,'*');
+    sp1_PrintAt(16,i,solid_att,GRASS);
+  sp1_PrintAt(15,0,finish_att,FINISH);
 }
---
+++ src/levels_graphics.asm
@@ -0,0 +1,34 @@
+SECTION rodata_user
+
+PUBLIC _grass1
+._grass1
+        defb @01001010
+	defb @01101110
+	defb @11001110
+	defb @11011110
+	defb @11111111
+	defb @11111111
+	defb @11111111
+	defb @11111111
+
+PUBLIC _jumper
+._jumper
+        defb @11111111
+        defb @11111111
+	defb @00011000
+	defb @01111110
+	defb @00011000
+	defb @00111100
+	defb @00011000
+	defb @00000000
+
+PUBLIC _finish
+._finish
+        defb @11111111
+        defb @10000001
+	defb @10111101
+	defb @10100101
+	defb @10100101
+	defb @10111101
+	defb @10000001
+	defb @11111110
