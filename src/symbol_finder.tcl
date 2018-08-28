8a70ae9cd2b2f7c786bce74d4e0d5b5a45799c68
Derek Fountain <derek@scratters.com>
2018-08-28 15:27:14 +0100

    add symbol finder script

--- src/symbol_finder.tcl
+++ src/symbol_finder.tcl
@@ -1,5 +1,22 @@
 #!/usr/bin/wish
 
+# Wonky One Key, a ZX Spectrum game featuring a single control key
+# Copyright (C) 2018 Derek Fountain
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 # Crude little script to present an interface to finding symbols
 # in my map file. Slightly quicker than grep...
 #
@@ -38,10 +55,10 @@ proc find_symbol { args } {
     .t insert 0.0 $result
 }
 
-pack [entry .e -width 20 -background white -textvariable findMe] -expand true -fill x
+pack [entry .e -width 20 -background white -textvariable findMe] -expand false -fill x
 bind .e <KeyRelease> find_symbol
 focus .e
-pack [text .t -width 50 -height 5 -background white] -side top
+pack [text .t -width 50 -height 5 -background white] -side top -expand true -fill both
 
 set ::last_modification 0
 readSymbols [lindex $argv 0]
