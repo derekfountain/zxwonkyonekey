218948f801e393a072c1d05ee0c281fcf5a9420c
Derek Fountain <derek@scratters.com>
2018-11-06 14:46:22 +0000

    add more to the memory free script

--- src/how_much_memory_left.sh
+++ src/how_much_memory_left.sh
@@ -1,5 +1,27 @@
 #!/bin/sh
 
+# Interesting command is:
+#  grep -P '__\w+size' wonky.map | grep -v \$0000
+
+CODE_SIZE=$(perl -ne '/^__code_compiler_size\s+=\s+\$(\w+)/ && print hex($1)' < wonky.map)
+
+echo -n "\nCompiled code size is       "
+printf "%5d  " $CODE_SIZE
+printf "(0x%04X) bytes\n" $CODE_SIZE
+
+DATA_SIZE=$(perl -ne '/^__data_compiler_size\s+=\s+\$(\w+)/ && print hex($1)' < wonky.map)
+
+echo -n "Compiled data size is       "
+printf "%5d  " $DATA_SIZE
+printf "(0x%04X) bytes\n" $DATA_SIZE
+
+RODATA_USER_SIZE=$(perl -ne '/^__rodata_user_size\s+=\s+\$(\w+)/ && print hex($1)' < wonky.map)
+
+echo -n "Read only user data size is "
+printf "%5d  " $RODATA_USER_SIZE
+printf "(0x%04X) bytes\n" $RODATA_USER_SIZE
+
+
 REG_SP=$(perl -ne '/^REGISTER_SP\s+=\s+\$(\w+)/ && print hex($1)' < wonky.map)
 BSS_END=$(perl -ne '/^__BSS_END_tail\s+=\s+\$(\w+)/ && print hex($1)' < wonky.map)
 STACK_SIZE=$(perl -ne '/^TAR__crt_stack_size\s+=\s+\$(\w+)/ && print hex($1)' < wonky.map)
@@ -7,15 +29,15 @@ STACK_SIZE=$(perl -ne '/^TAR__crt_stack_size\s+=\s+\$(\w+)/ && print hex($1)' < 
 TOP_STACK=$(expr $REG_SP - $STACK_SIZE)
 FREE_MEM=$(expr $TOP_STACK - $BSS_END)
 
-echo -n "\nStack comes down to "
+echo -n "\nStack comes down to         "
 printf "%5d  " $TOP_STACK
 printf "(0x%04X)\n" $TOP_STACK
 
-echo -n "Heap starts at      "
+echo -n "Heap starts at              "
 printf "%5d  " $BSS_END
 printf "(0x%04X)\n" $BSS_END
 
-echo "                    ====="
-echo -n "Free memory         "
+echo "                            ====="
+echo -n "Free memory                 "
 printf "%5d  " $FREE_MEM
 printf "(0x%04X)\n" $FREE_MEM
