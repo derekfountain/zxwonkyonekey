765b037e6cd6f3435a4e23050ecb7d23fe768f0d
Derek Fountain <derek@scratters.com>
2018-11-17 11:09:33 +0000

    set default level to 4

--- src/main.c
+++ src/main.c
@@ -75,7 +75,7 @@ int main()
 
   create_runner( RIGHT );
 
-  current_level_num = 3;
+  current_level_num = 4;
   while( 1 ) {
     
     /* Get the level data and call it's draw function to draw it */
