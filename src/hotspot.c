dc3c30ab7649a446defd1f6fb7f736d64b23e0fd
Derek Fountain <derek@scratters.com>
2019-02-05 19:16:29 +0000

    start the hotspot optimisation

--- src/hotspot.c
+++ src/hotspot.c
@@ -1,41 +1,31 @@
 /*
  * Wonky One Key, a ZX Spectrum game featuring a single control key
  * Copyright (C) 2018 Derek Fountain
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#ifndef __GAME_STATE_H
-#define __GAME_STATE_H
+#include "hotspot.h"
 
-#include <stdint.h>
-
-#include "levels.h"
-
-/*
- * This structure defines the state of the game.
- *
- */
-typedef struct _game_state
+void create_hotspot_list( GAME_STATE* game_state )
 {
-  uint8_t     key_pressed;
-  uint8_t     key_processed;
 
-  LEVEL_DATA* current_level;
-  struct _hotspot*    hotspot_list;
+}
 
-} GAME_STATE;
+void teardown_hotspot_list( GAME_STATE* game_state )
+{
 
-#endif
+}
+
--- src/hotspot.h
+++ src/hotspot.h
@@ -1,41 +1,39 @@
 /*
  * Wonky One Key, a ZX Spectrum game featuring a single control key
  * Copyright (C) 2018 Derek Fountain
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#ifndef __GAME_STATE_H
-#define __GAME_STATE_H
+#ifndef __HOTSPOT_H
+#define __HOTSPOT_H
 
 #include <stdint.h>
 
-#include "levels.h"
+#include "game_state.h"
 
-/*
- * This structure defines the state of the game.
- *
- */
-typedef struct _game_state
+typedef struct _hotspot
 {
-  uint8_t     key_pressed;
-  uint8_t     key_processed;
+  struct _hotspot* next;
 
-  LEVEL_DATA* current_level;
-  struct _hotspot*    hotspot_list;
+  uint8_t          x;
+  uint8_t          y;
+} HOTSPOT;
 
-} GAME_STATE;
+
+void create_hotspot_list( GAME_STATE* game_state );
+void teardown_hotspot_list( GAME_STATE* game_state );
 
 #endif
